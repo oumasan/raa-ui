99c4efc5e39eae440e8bbec576a151646bdcc14f
oumasan <n.m1204@icloud.com>
2024-02-15 22:21:16 +0900

    inital commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-24">
+      <div>
+        <h2>ようこそ仮webサイトへ</h2>
+      </div>
+      <Button>Click Here</Button>
+    </main>
+  );
+}
