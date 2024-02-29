3e9045003cc8868fcb6c2ca03841a756be8c40c1
oumasan <n.m1204@icloud.com>
2024-02-29 21:24:45 +0900

    デザイン微修正

--- components/ArticleDetail.tsx
+++ components/ArticleDetail.tsx
@@ -8,6 +8,8 @@ import {
 } from "@/components/ui/card"
 import Image from "next/image"
 import NoImage from "@/assets/images/no_image.jpeg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMoon, faWineGlass } from '@fortawesome/free-solid-svg-icons'
 
 const ArticleDetail = (props: any) => {
   const article = {...props.props}
@@ -29,6 +31,22 @@ const ArticleDetail = (props: any) => {
       </CardContent>
       <CardFooter>
         <CardTitle className="mb-3">{`${article.ramen} : ${article.price}`}</CardTitle>
+        <CardDescription className="mb-2 flex">
+          <div className="mr-2">
+            時間帯:&nbsp;
+            <FontAwesomeIcon icon={faMoon} bounce style={{color: "#FFD43B", display: article.timeZone !== "midnight" ? "none" : ""}} />
+          </div>
+          <div className="mr-2">
+            状態:&nbsp;
+            <FontAwesomeIcon icon={faWineGlass} bounce style={{color: "#B197FC",}} />
+          </div>
+        </CardDescription>
+        <CardDescription className="mb-3 flex">
+          <div className="mr-2 flex">
+            種類:&nbsp;
+            <div className="" style={{display: article.type !== "tonkotsu" ? "none" : ""}}>豚骨</div>
+          </div>
+        </CardDescription>
         <CardDescription className="tracking-wider break-words">{article.comment}</CardDescription>
       </CardFooter>
     </Card>
